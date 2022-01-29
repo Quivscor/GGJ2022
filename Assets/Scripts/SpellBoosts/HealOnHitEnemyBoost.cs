ebdb72ef59b41108339fc676e4f2ccaac913c559
Galapagos Guy <zawisza_mateusz@wp.pl>
2022-01-29 19:02:43 +0100

    Healing effect

--- Assets/Scripts/SpellBoosts/HealOnHitEnemyBoost.cs
+++ Assets/Scripts/SpellBoosts/HealOnHitEnemyBoost.cs
@@ -7,11 +7,18 @@ public class HealOnHitEnemyBoost : SpellBoost
 {
 	private float healingFactor = 0.25f;
 
+	private GameObject healingEffectParticles = null;
+	private GameObject healingEnemyEffectParticles = null;
+
     public HealOnHitEnemyBoost()
     {
 		costModifier = 0.1f;
 		description = "Every attack heals you by +25% of damage dealt";
+
+		healingEffectParticles = Resources.Load<GameObject>("HealingParticles");
+		healingEnemyEffectParticles = Resources.Load<GameObject>("HealingParticlesEnemy");
 	}
+
 	public override void ProcessSpellBoost(Spell spell)
 	{
 		spell.MissileHitCharacter += HealOnHitTarget;
@@ -20,5 +27,18 @@ public class HealOnHitEnemyBoost : SpellBoost
 	private void HealOnHitTarget(CharacterStats parent, CharacterStats target, float damage)
 	{
 		parent.Heal(damage * healingFactor);
+
+		GameObject spawnedParticles = null;
+
+		if (parent.TryGetComponent<PlayerInputController>(out PlayerInputController playerInputController))
+		{
+			spawnedParticles = MonoBehaviour.Instantiate(healingEffectParticles, parent.transform.position, Quaternion.identity);
+		}
+		else
+		{
+			spawnedParticles = MonoBehaviour.Instantiate(healingEnemyEffectParticles, parent.transform.position, Quaternion.identity);
+		}
+
+		MonoBehaviour.Destroy(spawnedParticles, 1.5f);
 	}
 }
