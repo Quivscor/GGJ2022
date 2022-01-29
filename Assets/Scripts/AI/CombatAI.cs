b18f5c6608d66f447ebca6a2738e6951c9d84fcb
Quivscor <quivscor@gmail.com>
2022-01-29 05:12:36 +0100

    Tried adding combat, failed

--- Assets/Scripts/AI/CombatAI.cs
+++ Assets/Scripts/AI/CombatAI.cs
@@ -6,4 +6,19 @@ public class CombatAI : MonoBehaviour
 {
     public List<Transform> enemyTargets;
     public List<float> enemyThreatGauge;
+
+    private WandController wand;
+    private CharacterRotator rotator;
+
+    private void Awake()
+    {
+        wand = GetComponent<WandController>();
+        rotator = GetComponent<CharacterRotator>();
+    }
+
+    private void Update()
+    {
+        wand.ProcessSpell(true, false);
+        rotator.LookAt(enemyTargets[0].position);
+    }
 }
