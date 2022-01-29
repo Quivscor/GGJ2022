60d0dea087a0fa389470fb78eb2417e799c912db
Galapagos Guy <zawisza_mateusz@wp.pl>
2022-01-29 12:30:20 +0100

    Applying damage

--- Assets/Scripts/SpellMissile.cs
+++ Assets/Scripts/SpellMissile.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SpellMissile : MonoBehaviour
 {
 	private GameObject parent = null;
+	private float damageToApply = 0.0f;
 
-	public void Initialize(GameObject parent)
+	public void Initialize(GameObject parent, float damageToApply)
 	{
 		this.parent = parent;
+		this.damageToApply = damageToApply;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.transform.root.gameObject == parent)
 			return;
+
+		if (other.transform.root.TryGetComponent(out CharacterStats characterStats))
+		{
+			characterStats.DealDamge(damageToApply);
+		}
 
 		Destroy(this.gameObject);
 	}
