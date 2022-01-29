6c1bff0d7b14ed267914a91ac893b39fd07f0a7d
Quivscor <quivscor@gmail.com>
2022-01-29 04:48:38 +0100

    Nicely working MovementAI

--- Assets/Scripts/AI/MovementAI.cs
+++ Assets/Scripts/AI/MovementAI.cs
@@ -10,32 +10,47 @@ public class MovementAI : MonoBehaviour
     public List<Transform> enemyTargets;
     public Transform activeTarget;
 
-    [SerializeField, Range(0, 5f)] float enemyBias;
-    [SerializeField] float prefEnemyDistance;
-    [SerializeField, Range(0, 5f)] float targetBias;
+    [SerializeField, Range(0, 1f)] float enemyBias;
+    [SerializeField] float enemyDistanceFactor;
+    /// <summary>
+    /// x is minimal preffered distance, y is maximal preffered distance
+    /// </summary>
+    [SerializeField] Vector2 prefEnemyDistance;
+    [SerializeField, Range(0, 1f)] float targetBias;
 
     public List<Transform> allyTargets;
 
-    [SerializeField, Range(0, 5f)] float allyBias;
+    [SerializeField, Range(0, 1f)] float allyBias;
 
     public Vector3 MoveDir { get; private set; }
 
-    [SerializeField, Range(0,5f)] float oldMoveBias;
+    [SerializeField, Range(0,1f)] float oldMoveBias;
 
     [SerializeField] Vector2 biasRange;
 
     private NavMeshAgent agent;
     private CharacterMovement charMovement;
+    private float colliderRadius;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         charMovement = GetComponent<CharacterMovement>();
+        colliderRadius = GetComponent<CapsuleCollider>().radius + 0.01f;
     }
+
+    [SerializeField] float aiUpdateFrequency = .5f;
+    float updateTimer = 0;
 
     private void Update()
     {
-        MoveDir = GetMoveDir();
+        if (updateTimer <= 0)
+        {
+            MoveDir = GetMoveDir();
+            updateTimer = aiUpdateFrequency;
+        }
+        else
+            updateTimer -= Time.deltaTime;
 
         charMovement.ProcessMovement(MoveDir.z, MoveDir.x);
     }
@@ -84,17 +99,27 @@ public class MovementAI : MonoBehaviour
         {
             //vector towards target
             Vector3 unitDistance = t.position - this.transform.position;
+            //RaycastHit hit;
+            //if(Physics.Raycast(this.transform.position + Vector3.up - (unitDistance * colliderRadius), t.position + Vector3.up, out hit))
+            //{
+            //    if(hit.transform != t)
+            //    {
+            //        //enemy is not in line of sight so ignore them
+            //        continue;
+            //    }
+            //}
             for (int i = 0; i < weights.Length; i++)
             {
                 float weightDelta = Vector2.Dot(VectorCast.CastVector3ToVector2(vectors[i]),
-                        VectorCast.CastVector3ToVector2(unitDistance)) * enemyBias;
+                        VectorCast.CastVector3ToVector2(unitDistance.normalized)) * 
+                        (enemyBias + ((1/unitDistance.magnitude) * enemyDistanceFactor));
                 //if enemy too far from target, walk up to it
-                if (unitDistance.magnitude > prefEnemyDistance)
+                if (unitDistance.magnitude > prefEnemyDistance.y)
                 {
                     weights[i] += weightDelta;
                 }
                 //if not, move away from them
-                else
+                else if(unitDistance.magnitude < prefEnemyDistance.x)
                 {
                     weights[i] -= weightDelta;
                 }
@@ -104,7 +129,9 @@ public class MovementAI : MonoBehaviour
 
     [SerializeField] float wallRaycastDistance;
     [SerializeField] LayerMask wallLayerMask;
+    [SerializeField] float wallWeightBonus;
     [SerializeField, Range(0, 5f)] float wallBias;
+    [SerializeField] float wallDistanceFactor;
 
     private void GetWallsRuleMovement(Vector3[] vectors, ref float[] weights)
     {
@@ -112,51 +139,69 @@ public class MovementAI : MonoBehaviour
         foreach(Vector3 dir in vectors)
         {
             RaycastHit hit;
-            if(Physics.Raycast(this.transform.position, dir, out hit, wallRaycastDistance, wallLayerMask))
+            Physics.Raycast(this.transform.position, dir, out hit, wallRaycastDistance, wallLayerMask);
+            hits.Add(hit);
+        }
+
+        float[] testWeights = new float[8];
+        int testCounter = 0;
+        for(int i = 0; i < weights.Length; i++)
+        {
+            if(hits[i].collider == null)
             {
-                hits.Add(hit);
+                //if wall not detected, nudge slightly in that direction
+                testWeights[i] += wallWeightBonus;
+                testCounter++;
+            }
+            else
+            {
+                //if wall detected, you don't want to go there, the closer you are the more you don't want to go there
+                weights[i] -= (wallRaycastDistance / Vector3.Distance(this.transform.position, hits[i].point)) * wallBias;
             }
         }
 
-        foreach(RaycastHit hit in hits)
+        if(testCounter < weights.Length)
         {
-            Vector3 unitDistance = Vector3.Normalize(this.transform.position - hit.point);
-            for (int i = 0; i < weights.Length; i++)
+            //not all directions returned no wall, so apply nudge in those directions
+            for(int i = 0; i < weights.Length; i++)
             {
-                //wants to move away from walls
-                weights[i] += Vector2.Dot(VectorCast.CastVector3ToVector2(vectors[i]),
-                    VectorCast.CastVector3ToVector2(unitDistance)) * wallBias;
+                weights[i] += testWeights[i];
             }
         }
     }
 
     [SerializeField] float bulletDetectionRange;
     [SerializeField] float bulletBias;
+    [SerializeField] string bulletTag;
     private void GetDodgeBulletsRuleMovement(Vector3[] vectors, ref float[] weights)
     {
         Collider[] colliders = Physics.OverlapSphere(this.transform.position, bulletDetectionRange);
 
         foreach(Collider col in colliders)
         {
-            //check collider tag
-
-            Vector3 unitDistance = Vector3.Normalize(col.transform.position - this.transform.position);
-
-            for(int i = 0; i < weights.Length; i++)
+            if(col.CompareTag(bulletTag))
             {
-                weights[i] += Vector2.Dot(VectorCast.CastVector3ToVector2(vectors[i]),
-                    VectorCast.CastVector3ToVector2(unitDistance)) * bulletBias;
+                Vector3 unitDistance = Vector3.Normalize(col.transform.position - this.transform.position);
+
+                for (int i = 0; i < weights.Length; i++)
+                {
+                    //dodge bullets
+                    weights[i] -= Vector2.Dot(VectorCast.CastVector3ToVector2(vectors[i]),
+                        VectorCast.CastVector3ToVector2(unitDistance)) * bulletBias;
+                }
             }
         }
     }
 
+    float[] oldWeights = new float[8];
     private void GetOldMoveRuleMovement(Vector3[] vectors, ref float[] weights)
     {
         for(int i = 0; i < weights.Length; i++)
         {
-            weights[i] += Vector2.Dot(VectorCast.CastVector3ToVector2(vectors[i]),
-                    VectorCast.CastVector3ToVector2(MoveDir.normalized)) * oldMoveBias;
+            if(weights[i] != 0)
+                weights[i] += oldWeights[i] * oldMoveBias;
         }
+        oldWeights = weights;
     }
 
     private Vector3[] GetSteeringBehaviorWeights()
@@ -176,7 +221,8 @@ public class MovementAI : MonoBehaviour
         if(debugDrawRange)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(this.transform.position, prefEnemyDistance);
+            Gizmos.DrawWireSphere(this.transform.position, prefEnemyDistance.x);
+            Gizmos.DrawWireSphere(this.transform.position, prefEnemyDistance.y);
 
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(this.transform.position, wallRaycastDistance);
